afa8fd11c65053104c818aadabef41273feb5d47
Nguyen Huynh Khanh Duy <nhkduy201@gmail.com>
2021-04-24 11:35:31 +0700

    remove unnecessary getch.py

--- getch.py
+++ getch.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
 try:
     import msvcrt
-    # def key_pressed():
-    #   return msvcrt.kbhit()
     def read_key():
       key = msvcrt.getch()
       try:
@@ -13,20 +11,7 @@ try:
 except:
   try:
     import sys
-    # import select
-    # import tty
-    # import termios
-    # import atexit
-
-    # def key_pressed():
-    #     return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
     def read_key():
         return sys.stdin.read(1)
-    # def restore_settings():
-    #     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-
-    # atexit.register(restore_settings)
-    # old_settings = termios.tcgetattr(sys.stdin)
-    # tty.setcbreak(sys.stdin.fileno())
   except:
     raise Exception("Can't deal with your keyboard!")
